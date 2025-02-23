464ac18095361daef3309fae0e1dd51b4a11c29e
bvp13 <bvp13@BhushanPatil>
2025-02-23 14:00:28 +0530

    this is last

--- src/AccessSpecifiers1/Example8.java
+++ src/AccessSpecifiers1/Example8.java
@@ -6,10 +6,10 @@ import AccessSpecifiers.Example6;            // protected access specifier
 public class Example8 extends Example6
 
 {
-public static void main(String[] args) 
-{
-	Example8 s8 = new Example8();
-   System.out.println(s8.c);
-   s8.printsquarenum();
-}
+	public static void main(String[] args) 
+	{
+		Example8 s8 = new Example8();
+		System.out.println(s8.c);
+		s8.printsquarenum();
+	}
 }
--- src/Collection_Practice/Example2_Vector.java
+++ src/Collection_Practice/Example2_Vector.java
@@ -9,7 +9,7 @@ public class Example2_Vector
 
 	public static void main(String[] args) 
 	{
-	
+
 		Vector v = new Vector();
 		v.add("bhushan");
 		v.add(102);
@@ -20,29 +20,29 @@ public class Example2_Vector
 		v.add(null);
 		v.add(null);
 		v.add(null);
-		
+
 		System.out.println(v.get(1));
 		System.out.println(v.get(8));
 		System.out.println(v.isEmpty());
 		System.out.println(v.contains('d'));
-		
+
 
 		System.out.println("----print data using  ListIterator cursor----");
-		
+
 		ListIterator l = v.listIterator();
 		while(l.hasNext())
 		{
 			System.out.println(l.next());
 		}
-		
+
 		System.out.println("----print data using for Enumeration cursor----");
-		
+
 		Enumeration e = v.elements();
 		while(e.hasMoreElements())
 		{
 			System.out.println(e.nextElement());
 		}
-		
+
 		System.out.println("----print data using  ListIterator cursor----");
 
 		ListIterator h = v.listIterator();
@@ -50,19 +50,19 @@ public class Example2_Vector
 		{
 			System.out.println(h.next());
 		}
-		
+
 		//h.add("kuldeep");
 		System.out.println(v);
-		
+
 		//h.remove();
 		System.out.println(v);
-		
+
 		h.set("dheeraj");
 		System.out.println(v);
-		
-		
-		
-		
+
+
+
+
 	}
 
 }
